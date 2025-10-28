2079b7fa8bd47ee46e106baa6ca21d30ba9995f9
naghemanth57-ai <naghemanth57@gmail.com>
2025-10-28 15:19:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
+import streamlit as st
+import cv2
+import pandas as pd
+from ultralytics import YOLO
+from PIL import Image
+import numpy as np
+import os
 streamlit
 ultralytics
-opencv-python-headless==4.10.0.84
 pandas
 Pillow
 
